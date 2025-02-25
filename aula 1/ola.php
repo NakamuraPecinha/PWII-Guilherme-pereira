b0a28552b2f563b77093aa611f4f10728fab256c
NakamuraPecinha <pereirablaze0@gmail.com>
2025-02-25 16:34:23 -0300

    Aula de PHP com exemplos de concatenação de variáveis

---
+++ aula 1/ola.php
@@ -0,0 +1,12 @@
+
+        <?php
+            echo "Ola mundo ! <br>" ;
+            $Nome = "Guilherme ";
+            $sobrenome = "Nakamura";
+            $NUMERO = 69;
+
+            ECHO "${Nome} <h1> ${sobrenome} </h1>";
+            ECHO $NUMERO . "<p>" . $Nome . "" . $sobrenome . "</p>";
+?>
+
+    
