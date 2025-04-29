a65953f7df2ee424ee547faa29509205410d5c86
NakamuraPecinha <pereirablaze0@gmail.com>
2025-04-29 16:28:46 -0300

    aula de php

---
+++ aula-4/lacos.php
@@ -0,0 +1,25 @@
+<?php 
+  //while
+  $contador = 1;
+
+  while ($i < 6) {
+   echo $contador, "<br>";
+   $contador++;
+  }
+  echo "Do while <br>";
+  
+  //do while
+  $contadorii = 1;
+
+  do {
+    echo $contadorii;
+    $contadorii++;
+  } while ($contadorii < 6);
+
+  //For 
+  echo "<br> For <br>";
+
+  for ($x = 0; $x <= 10; $x++) {
+    echo "O número é $x <br>";
+  }
+?>
